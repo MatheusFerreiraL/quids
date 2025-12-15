e901c4dfa26db8ae863cc969f427e03cd3b6aa76
MatheusFerreiraL <matheusfleandro@gmail.com>
2025-12-15 19:35:24 +0000

    init: project structure

---
+++ src/main/java/github/matheusferreiral/quids/QuidsApplication.java
@@ -0,0 +1,13 @@
+package github.matheusferreiral.quids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuidsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuidsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/github/matheusferreiral/quids/QuidsApplicationTests.java
@@ -0,0 +1,13 @@
+package github.matheusferreiral.quids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuidsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
